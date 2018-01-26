0b6afa253aefc869a8a64963ed8ce8de84468aa4
zhuolinho <zhuolinho@hotmail.com>
2018-01-26 12:04:33 +0800

    truffle-contract

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var MetaCoin = require('../build/contracts/MetaCoin.json');
+var Web3 = require('web3');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send(MetaCoin);
 });
 
 module.exports = router;
